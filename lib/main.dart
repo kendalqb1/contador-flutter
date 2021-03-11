199d9db4d94db17a8d7a79f279fefc096d8e162d
kendalqb1 <kendalqb1@gmail.com>
2021-03-10 18:11:12 -0600

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// Importaciones de dart
+import 'package:flutter/material.dart';
+
+// Importaciones de nuestros archivos
+// Import 'src/app.dart';
+import 'package:contador/src/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
